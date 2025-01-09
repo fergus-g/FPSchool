7488ba0d95d0d8cd837d13c876af0175bbbdf104
fergus-g <fergus.gildea@utrack.com>
2025-01-09 16:49:44 +0000

    Added outlined functions

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-async function showLeaderBoard() {}
+// GET REQUEST
+async function showLeaderBoard() {
+  // calls the api
+  // Displays all the results as li in the content container
+  //Hide the edit and delete buttons
+}
+
+// GET REQUEST
+async function showAllPlayers() {
+  // calls the api
+  // Displays all the results as li in the content container
+  // show the edit and delete buttons
+}
+
+// POST REQUEST
+async function createNewPlayer() {
+  // calls the api
+  // Opens a prompt to input player details
+  // takes details and posts to API
+}
+
+// POST REQUEST
+async function createNewMap() {
+  // calls the api
+  // Opens a prompt to input map details
+  // takes details and posts to API
+}
+
+// PATCH REQUEST
+
+async function editPlayer() {
+  // calls the api
+  // Opens a prompt to input update to player details
+  // takes details and posts to API
+}
+
+// PATCH REQUEST
+
+async function editPlayer() {
+  // calls the api
+  // Opens a prompt to input update to map details
+  // takes details and posts to API
+}
+
+// DELETE REQUEST
+
+async function deletePlayer() {
+  // gets the id of the specific user and passes to the API call at id
+}
+
+// DELETE REQUEST
+
+async function deleteMap() {
+  // gets the id of the specific map and passes to the API call at id
+}
